17f5b25fa37705cfee3c930bef1ba2f5223a0db9
Anit2838 <ag5316484359@gmail.com>
2024-08-26 12:38:45 +0530

    Add files via upload

---
+++ python_day3_operator_assigment1.py
@@ -0,0 +1,15 @@
+# Taking input from the user
+num = int(input("Enter a number: "))
+
+# Using a ternary operator to check if the number is even or odd
+result = "Even" if num % 2 == 0 else "Odd"
+
+# Printing the result
+print(f"The number is {result}.")
+
+'''OUTPUT
+Enter a number: 5
+The number is Odd.
+
+Enter a number: 6
+The number is Even.'''
---
+++ python_day3_operator_assigment2.py
@@ -0,0 +1,18 @@
+# Taking two numbers from the user
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+# Swapping the numbers
+num1, num2 = num2, num1
+
+# Printing the swapped values
+print("After swapping:")
+print("First number:", num1)
+print("Second number:", num2)
+
+'''OUTPUT
+Enter the first number: 5
+Enter the second number: 8
+After swapping:
+First number: 8
+Second number: 5'''
